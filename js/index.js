53ddffbc03b75d166439045073e16c762e8a2926
victoria29 <vmartynova99@gmail.com>
2017-05-19 17:48:41 +0300

    changes

--- js/index.js
+++ js/index.js
@@ -1,18 +1,16 @@
 $(function () {
     $('form input[type=submit]').click(sendForm);
-
 })
 function sendForm(e) {
     e.preventDefault();
     $.ajax({
         url: "https://formspree.io/vmartynova99@gmail.com",
         method: "POST",
-        data: {clientNname: $('#client-name').val()},
+        data: {clientName: $('#client-name').val()},
         dataType: "json"
     }).done (function () {
         $('form').html('<h1> Дякуємо! З Вами неодмінно зв*яжуться!</h1>');
     }).fail(function () {
         $('form').html('<h1> Виникла помилка! Спробуйте пізніше.</h1>');
-
     });
 }
